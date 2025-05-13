5e81717302c80192b2ec963fbf8bab6d66445668
Gleb <fedorov22134@gmail.com>
2025-05-13 23:28:29 +0300

    add task background (to test)

--- src/ftk/router.py
+++ src/ftk/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, BackgroundTasks
 
 from src.config.settings import settings
 from src.ftk.dependencies import get_result_ftk_parser_service, get_ftk_repository
@@ -10,9 +10,11 @@ router = APIRouter(prefix=settings.url.prefix_ftk, tags=[settings.url.tag_ftk])
 
 @router.get("/start_parser")
 async def start_parser(
+    background_tasks: BackgroundTasks,
     repo: Repository = Depends(get_ftk_repository)
 ):
-    return await get_result_ftk_parser_service(repo)
+    background_tasks.add_task(get_result_ftk_parser_service, repo)
+    return {"message": "success starting"}
 
 @router.get("/get_all")
 async def get_all_data_from_parsing(
