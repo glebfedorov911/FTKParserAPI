fa35c349be98ac907270430a952b87be6e09ecd0
Gleb <fedorov22134@gmail.com>
2025-05-20 16:27:10 +0300

    add new data

--- src/ftk/service.py
+++ src/ftk/service.py
@@ -53,17 +53,53 @@ class FTKParserService:
 
     @staticmethod
     def _replace_spec(field):
-        return (field.strip()
-                .replace("\t", " ")
-                .replace("\n", " ")
-                .replace(":", " ")
-                .replace("\\", " ")
-                .replace(",", " ")
-                .replace(";", " ")
-                .replace(".", " ")
-                .replace("-", " ")
-                .replace("_", " ")
+        translit_dict = {
+            'А': 'A', 'а': 'a',
+            'Б': 'B', 'б': 'b',
+            'В': 'V', 'в': 'v',
+            'Г': 'G', 'г': 'g',
+            'Д': 'D', 'д': 'd',
+            'Е': 'E', 'е': 'e',
+            'Ё': 'E', 'ё': 'e',
+            'Ж': 'Zh', 'ж': 'zh',
+            'З': 'Z', 'з': 'z',
+            'И': 'I', 'и': 'i',
+            'Й': 'Y', 'й': 'y',
+            'К': 'K', 'к': 'k',
+            'Л': 'L', 'л': 'l',
+            'М': 'M', 'м': 'm',
+            'Н': 'N', 'н': 'n',
+            'О': 'O', 'о': 'o',
+            'П': 'P', 'п': 'p',
+            'Р': 'R', 'р': 'r',
+            'С': 'S', 'с': 's',
+            'Т': 'T', 'т': 't',
+            'У': 'U', 'у': 'u',
+            'Ф': 'F', 'ф': 'f',
+            'Х': 'Kh', 'х': 'kh',
+            'Ц': 'Ts', 'ц': 'ts',
+            'Ч': 'Ch', 'ч': 'ch',
+            'Ш': 'Sh', 'ш': 'sh',
+            'Щ': 'Shch', 'щ': 'shch',
+            'Ъ': '', 'ъ': '',
+            'Ы': 'Y', 'ы': 'y',
+            'Ь': '', 'ь': '',
+            'Э': 'E', 'э': 'e',
+            'Ю': 'Yu', 'ю': 'yu',
+            'Я': 'Ya', 'я': 'ya',
+        }
+        transliterated = ''.join([translit_dict.get(c, c) for c in field])
+        return (transliterated.strip()
+                .replace("\t", "")
+                .replace("\n", "")
+                .replace(":", "")
+                .replace("\\", "")
+                .replace(",", "")
+                .replace(";", "")
+                .replace(".", "")
+                .replace("-", "")
+                .replace("_", "")
                 .replace("(", "")
                 .replace(")", "")
-                .replace("/", " ")
+                .replace("/", "")
                 )
